6afd51651208c9562d13676fe3520a7e60010384
Breaveluv <nam.bule12@gmail.com>
2024-12-05 21:09:30 +0700

    de 1

--- HN_JPN_KS24B_NguyenVietNam_05.cpp
+++ HN_JPN_KS24B_NguyenVietNam_05.cpp
@@ -4,7 +4,7 @@
     int choice = 0;
     int length=0;
     int add = 0;
-    int delete=0;
+    int delete1=0;
     int a[100];
     int addNumber;
     int deleteNumber;
@@ -23,31 +23,31 @@
         printf("9. Sap xep lai mang va hien thi ra toan bo phan tu co trong mang sao cho toan bo so le đung truoc, so chan đung sau\n");
         printf("10. Đao nguoc thu tu của các phan tu trong mang va hien thi toan bo\n");
         printf("11. Thoát\n");
-        printf("Hãy nhập lựa chọn của bạn : ");
+        printf("Hãy nhap lua chon cua ban : ");
         scanf("%d",&choice);
         if (choice==1){
-            printf("Hãy nhập độ dài mảng : ");
+            printf("Hay nhap do dai cua mang : ");
             scanf("%d",&length);
-            for(int i = 0;i<(length+add-delete);i++){
-                printf("Hãy nhập phần tử a[%d] : ",i);
+            for(int i = 0;i<(length+add-delete1);i++){
+                printf("Nhap phan tu  a[%d] : ",i);
                 scanf("%d",&a[i]);
             }
         }else if(choice==2){
             if(length==0){
-                printf("\nBạn chưa nhập phần tử cho mảng\n\n");
+                printf("\nChua nhap phan tu mang\n\n");
             }else{
-                printf("Các phần tử của mảng là:\n");
-                for(int i = 0;i<(length+add-delete);i++){
+                printf("Cac phan tu cua mang:\n");
+                for(int i = 0;i<(length+add-delete1);i++){
                     
                     printf("a[%d] = %d\n",i,a[i]);
                 }
             }
         }else if(choice==3){
             if(length==0){
-                printf("\nBạn chưa nhập phần tử cho mảng\n\n");
+                printf("\nBan chua nhap phan tu cua mang\n\n");
             }else{
                 int condition=0;
-                for(int i = 0;i<(length+add-delete);i++){
+                for(int i = 0;i<(length+add-delete1);i++){
                     int perfect=0;
                     for(int j = 1;j<a[i];j++){
                         if(a[i]%j==0){
@@ -55,21 +55,21 @@
                         }
                     }
                     if(perfect==a[i]){
-                        printf("\nSố hoàn hảo có trong mảng là : %d\n",a[i]);
+                        printf("\n So hoan hao trong mang la : %d\n",a[i]);
                         condition++;
                     }
                 }
                 if(condition==0){
-                    printf("\nKhông có số hoàn hảo trong mảng\n\n");
+                    printf("\nKhong co so hoan hao trong mang\n\n");
                 }
             }
         }else if(choice == 4){
             if(length==0){
-                printf("\nBạn chưa nhập phần tử cho mảng\n\n");
+                printf("\nBan chua nhap phan tu cho mang\n\n");
             }else{
                 int max=0;
                 int temp;
-                for(int i =1;i<(length+add-delete);i++){
+                for(int i =1;i<(length+add-delete1);i++){
                     if(a[i]>a[max]){
                         max=i;
                         temp = a[i];
@@ -77,7 +77,7 @@
                 }
                 a[max]=0;
                 int MAX=0;
-                for(int i = 0;i<(length+add-delete);i++){
+                for(int i = 0;i<(length+add-delete1);i++){
                     if(a[i]>a[MAX]){
                         MAX=i;
                     }
@@ -90,7 +90,7 @@
             if(length==0){
                 printf("\nBạn chưa nhập phần tử cho mảng\n\n");
             }else{
-                for(int i=(length+add-delete);i>0;i--){
+                for(int i=(length+add-delete1);i>0;i--){
                     a[i]=a[i-1];
                     
                 }
@@ -105,10 +105,10 @@
             }else{
                 printf("Hãy chọn vị trí bạn muốn xóa phần tử : ");
                 scanf("%d",&deleteNumber);
-                for(int i=deleteNumber;i<(length+add-delete);i++){
+                for(int i=deleteNumber;i<(length+add-delete1);i++){
                     a[i]=a[i+1];
                 }
-                delete++;
+                delete1++;
             }
         }    
     }
